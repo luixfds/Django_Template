feadd796bd64b89b9c64eb3737f9f119f68d86ca
luixfds <l.fds@yahoo.com>
2021-11-17 08:25:34 -0300

    views finalized

--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
 from django.views.generic import TemplateView
 
+from home.models import Servicos, Equipe
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['dados servicos'] = Servicos.objects.order_by('?').all()
+        context['dados equipe'] = Equipe.objects.all()
+        return context
